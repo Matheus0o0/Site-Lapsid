2788866d8208a1ad4671236d03112d0bfbc97272
Pedro Lucas <pes.ex20000@gmail.com>
2025-04-09 21:49:58 -0300

    conteudo da home quase finalizado, footer adicionado(tem que ser ajustado)

---
+++ FrontEnd/lapsidfront/src/app/Components/Footer.tsx
@@ -0,0 +1,16 @@
+import style from "../Style/Footer.module.css";
+import Image from "next/image";
+
+export default function Footer() {
+    return(
+        <footer className={style.footer}>
+            <Image className={style.logoIf} src="logos/Rectangle6.svg" alt="IFBA Logo" width={500} height={500}/>
+            <p className={style.textFooter}><b>Todos os direitos reservados</b></p>
+            <ul className={style.listFooter}>
+                <li className={style.phone}>(55) 9999-999</li>
+                <li className={style.insta}>@Lapsid</li>
+                <li className={style.email}>Lapsid@gmail.com.br</li>
+            </ul> 
+        </footer>
+    );
+}
