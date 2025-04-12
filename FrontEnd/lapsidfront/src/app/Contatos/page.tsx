1dfe00f86132c9931d6359a8bff83f9d05fb342b
Pedro Lucas <pes.ex20000@gmail.com>
2025-04-12 10:48:24 -0300

    pagina de contatos pronta

---
+++ FrontEnd/lapsidfront/src/app/Contatos/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Map from '../Components/Map';
+import style from '../Style/Contatos.module.css';
+export default function Contatos() {
+    return (
+        <main>
+            <h1 className={style.title}>Contatos</h1>
+            <section className={style.sectionCont}>
+                    <div className={style.divCont}>
+                        <h3><b>Endereço</b></h3>
+                        <ul className={style.ulCont}>
+                            <li className={style.liCont} >BR-324, Km 521 - Aviário, Feira de Santana - BA, 44096-486</li>
+                        </ul>
+                        <h3><b>Telefone</b></h3>
+                        <ul className={style.ulCont}>
+                            <li className={style.liCont}>(75) 9 9999-999</li>
+                        </ul>
+                        <h3><b>Email</b></h3>
+                        <ul >
+                            <li className={style.liCont}>Null@gmail.com</li>
+                        </ul>
+                        <Map/>
+                    </div>
+            </section>
+
+        </main>
+    );
+}
---
+++ FrontEnd/lapsidfront/src/app/Projetos/page.tsx
@@ -0,0 +1,30 @@
+import style from '../Style/Projetos.module.css';
+import Image from 'next/image';
+
+export default function Projetos() {
+    return (
+        <main>
+            <h1 className={style.title}>Projetos</h1>
+            <section>
+                <div className={style.prj}>
+                    <h2 className={style.h2Prj}>Projeto 1</h2>
+                    <div className={style.divPrj}>
+                        <Image className={style.imgPrj} src={"CardImgs/Frame3.svg"} alt="Projeto 1" width={100} height={100}/>
+                        <p className={style.pPrj}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, quaerat, omnis fugit at voluptas voluptatum tempora deleniti perferendis alias expedita quos nobis dolores soluta, iusto aperiam dolorum error? Earum vel hic quisquam! Mollitia similique dignissimos eligendi molestias accusantium eveniet minima.</p>
+                    </div>
+                </div>
+                <div className={style.divHr}>
+                    <hr className={style.hr}/>
+                </div>
+                <div className={style.prj}>
+                    <h2 className={style.h2Prj}>Projeto 2</h2>
+                    <div className={style.divPrj}>
+                        <Image className={style.imgPrj} src={"CardImgs/Frame3.svg"} alt="Projeto 1" width={100} height={100}/>
+                        <p className={style.pPrj}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, quaerat, omnis fugit at voluptas voluptatum tempora deleniti perferendis alias expedita quos nobis dolores soluta, iusto aperiam dolorum error? Earum vel hic quisquam! Mollitia similique dignissimos eligendi molestias accusantium eveniet minima.</p>
+                    </div>
+                </div>
+
+            </section>
+        </main>
+    );
+}
